3d3534b44c7e0a1c60098cb553b62bf4af99dcfd
dxh_vip <dongxiaohui_vip@163.com>
2018-05-07 11:43:19 +0800

    Add files via upload

--- js/cannon.js
+++ js/cannon.js
@@ -1,42 +1,54 @@
 //炮
-var CANNON_SIZE=[
-	null,
-	{w: 74, h: 74},
-	{w: 74, h: 76},
-	{w: 74, h: 76},
-	{w: 74, h: 83},
-	{w: 74, h: 85},
-	{w: 74, h: 90},
-	{w: 74, h: 94}
+var CANNON_SIZE = [
+  null,
+  { w: 74, h: 74 },
+  { w: 74, h: 76 },
+  { w: 74, h: 76 },
+  { w: 74, h: 83 },
+  { w: 74, h: 85 },
+  { w: 74, h: 90 },
+  { w: 74, h: 94 }
 ];
+// ??????
+var oCwidth = getClientWidth();
+// ??????
+var oCheight = getClientHeight();
 
-function Cannon(type){
-	this.type=type;
-	this.rotate=90;
-	this.x=400;
-	this.y=360;
-	this.w=CANNON_SIZE[this.type].w;
-	this.h=CANNON_SIZE[this.type].h;
-	this.cur=0;
-	this.timer=null;
+function Cannon(type) {
+  this.type = type;
+  this.rotate = 90;
+	this.x = oCwidth / 2 + CANNON_SIZE[this.type].w/2;
+	this.y = oCheight - CANNON_SIZE[this.type].h/2+10;
+  this.w = CANNON_SIZE[this.type].w;
+  this.h = CANNON_SIZE[this.type].h;
+  this.cur = 0;
+  this.timer = null;
 }
-Cannon.prototype.draw=function(gd){		
-
-	gd.save();
-	gd.translate(this.x,this.y);
-	gd.rotate(d2a(this.rotate));	
-	gd.drawImage(JSON['cannon'+this.type],0,this.cur*this.h,this.w,this.h,-this.w/2,-this.h/2,this.w,this.h);
-	gd.restore();
-		
+Cannon.prototype.draw = function(gd) {
+  gd.save();
+  gd.translate(this.x, this.y);
+  gd.rotate(d2a(this.rotate));
+  gd.drawImage(
+    JSON['cannon' + this.type],
+    0,
+    this.cur * this.h,
+    this.w,
+    this.h,
+    -this.w / 2,
+    -this.h / 2,
+    this.w,
+    this.h
+  );
+  gd.restore();
 };
-Cannon.prototype.emitChange=function(){
-	var _this=this;
-	clearInterval(this.timer);
-	this.timer=setInterval(function(){
-		_this.cur++;
-		if(_this.cur==5){
-			_this.cur=0;
-			clearInterval(_this.timer);	
-		}	
-	},50);	
+Cannon.prototype.emitChange = function() {
+  var _this = this;
+  clearInterval(this.timer);
+  this.timer = setInterval(function() {
+    _this.cur++;
+    if (_this.cur == 5) {
+      _this.cur = 0;
+      clearInterval(_this.timer);
+    }
+  }, 50);
 };
--- js/com.js
+++ js/com.js
@@ -1,41 +1,58 @@
-var JSON={};
+var JSON = {};
+
+var html = document.getElementsByTagName('html')[0];
 
 //d2a
-function d2a(n){
-	return n*Math.PI/180;	
+function d2a(n) {
+  return n * Math.PI / 180;
 }
 
 //a2d
-function a2d(n){
-	return n*180/Math.PI;	
+function a2d(n) {
+  return n * 180 / Math.PI;
 }
 
 //随机数
-function rnd(m,n){
-	return parseInt(Math.random()*(m-n)+n);	
+function rnd(m, n) {
+  return parseInt(Math.random() * (m - n) + n);
 }
 
 //加载游戏资源
-function loadImage(arr,fnSucc,fnLoad){
-	var count=0;
-	for(var i=0; i<arr.length;i++){
-		var oImage=new Image();
-		(function(index){
-			oImage.onload=function(){
-				count++;		
-				
-				//保存资源
-				JSON[arr[index]]=this;
-				
-				fnLoad && fnLoad(count,arr.length);
-				
-				if(count==arr.length){
-					fnSucc && fnSucc();	
-				}
-			
-			};	
-		})(i);
-			
-		oImage.src='img/'+arr[i]+'.png';
-	};	
+function loadImage(arr, fnSucc, fnLoad) {
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    var oImage = new Image();
+    (function(index) {
+      oImage.onload = function() {
+        count++;
+
+        //保存资源
+        JSON[arr[index]] = this;
+
+        fnLoad && fnLoad(count, arr.length);
+
+        if (count == arr.length) {
+          fnSucc && fnSucc();
+        }
+      };
+    })(i);
+
+    oImage.src = 'img/' + arr[i] + '.png';
+  }
+}
+
+function getClientHeight() {
+  var clientHeight = 0;
+  if (html.clientHeight) {
+    var clientHeight = html.clientHeight;
+  }
+  return clientHeight;
+}
+
+function getClientWidth() {
+  var clientWidth = 0;
+  if (html.clientWidth) {
+    var clientWidth = html.clientWidth;
+  }
+  return clientWidth;
 }
